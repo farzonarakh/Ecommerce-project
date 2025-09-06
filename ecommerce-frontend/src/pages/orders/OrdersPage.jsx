bdccf05e3905b2248ca65cc33d0f78d0ca8e4e73
Farzona <farzonarakh@users.noreply.github.com>
2025-09-06 12:19:18 +0500

    Separate into smaller components

--- ecommerce-frontend/src/pages/orders/OrdersPage.jsx
+++ ecommerce-frontend/src/pages/orders/OrdersPage.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { useState, useEffect, Fragment } from 'react';
 import './OrdersPage.css';
-import { Header } from '../components/Header';
+import { Header } from '../../components/Header';
 import { Link } from 'react-router';
-import BuyAgain from '../assets/images/icons/buy-again.png';
+import BuyAgain from '../../assets/images/icons/buy-again.png';
 import dayjs from 'dayjs';
-import { formatMoney } from '../utils/money';
+import { formatMoney } from '../../utils/money';
 
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
