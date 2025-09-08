38cebb32319c2b0aba48895db1b8a56b85bb7a84
Farzona <farzonarakh@users.noreply.github.com>
2025-09-08 15:28:23 +0500

    Create delete cart item feature

--- ecommerce-frontend/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-frontend/src/pages/checkout/OrderSummary.jsx
@@ -7,13 +7,14 @@ export function OrderSummary({ deliveryOptions, cart, loadCart
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => { 
+
         return (
           <div key={cartItem.productId} className="cart-item-container">
             <DeliveryDate deliveryOptions={deliveryOptions} cartItem={cartItem}/>
 
             <div className="cart-item-details-grid">
 
-              <CartItemDetails cartItem={cartItem}/>
+              <CartItemDetails cartItem={cartItem} loadCart={loadCart} />
 
               <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart={loadCart}/>
             </div>
